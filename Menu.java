165c03c4ab90fe183245ea96f8e569cd3506128c
Venkata Harish Kajur <vk255@njit.edu>
2015-05-01 12:43:49 -0400

    Almost finish with student functions only drop course left

--- Menu.java
+++ Menu.java
@@ -1,11 +1,14 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Menu {
 
     private static Scanner scanner;
-    private static MysqlConnect mysqlconn;
+    private static MysqlConnect connect;
 
-    public static void mainMenu(){
+    private static final String[] ENROLLMENT_COLUMNS = {"Student", "Term", "SecNum", "Grade"};
+
+    public static void showMainMenu(){
 
         System.out.println("Main Menu");
         System.out.println("(1) Students functions");
@@ -13,6 +16,45 @@ public class Menu {
         System.out.println("(3) Reporting functions");
         System.out.println("(4) Quit");
 
+    }
+
+    public static void showStudentMenu(){
+
+        System.out.println("Student Functions Menu");
+        System.out.println("(1) Register for courses");
+        System.out.println("(2) Add/Drop a course");
+        System.out.println("(3) Request Transcript");
+        System.out.println("(4) Compute GPA");
+        System.out.println("(5) Quit");
+
+    }
+
+    public static void showAdministrativeMenu(){
+
+        System.out.println("Administrative Functions Menu");
+        System.out.println("(1) Create a new course/drop course");
+        System.out.println("(2) Prepare term schedule (add sections)");
+        System.out.println("(3) Add/drop instructors");
+        System.out.println("(4) Alter term schedule (add/drop/update sections)");
+        System.out.println("(5) Add/drop students");
+        System.out.println("(6) Print the list of instructors and the number of courses each of them is teaching");
+        System.out.println("(7) Quit");
+
+    }
+
+    public static void showReportingMenu(){
+
+        System.out.println("Reporting Functions Menu");
+        System.out.println("(1) Print schedule of classes (for a term)");
+        System.out.println("(2) Print the catalog");
+        System.out.println("(3) Print the honors list of students for a department (e.g. students who GPA > 3.8)");
+        System.out.println("(4) Quit");
+
+    }
+
+    public static void mainMenu(){
+
+        showMainMenu();
         int option = scanner.nextInt();
 
         while(true){
@@ -27,13 +69,18 @@ public class Menu {
                     option = scanner.nextInt();
                     break;
                 case 3:
+                    reportingMenu();
                     option = scanner.nextInt();
                     break;
                 case 4:
-                    System.exit(0);
+                    try {
+                        connect.close();
+                        System.exit(0);
+                    } catch(IOException io){
+                        System.err.println(io);
+                    }
                     break;
                 default:
-                    System.err.println("Invalid option");
                     System.err.println("Invalid option, please enter again");
                     option = scanner.nextInt();
                     break;
@@ -42,46 +89,109 @@ public class Menu {
 
     }
 
+    public static void addCourse(){
+
+        System.out.print("Enter student id: " );
+        String id = scanner.next();
+
+        System.out.print("Enter term to register: ");
+        String term = scanner.next();
+
+        System.out.print("Enter Section Number to register: " );
+        String secNum = scanner.next();
+
+        Object[] columnValues = new Object[4];
+
+        columnValues[0] = new Integer(id);
+        columnValues[1] = term;
+        columnValues[2] = new Integer(secNum);
+        columnValues[3] = new Integer(-1);
+
+        connect.insert("CS434_Enrollment", ENROLLMENT_COLUMNS, columnValues);
+        System.out.println("Course Successfully Registered");
+
+    }
+
+    public static void registerCourses(){
+
+        System.out.print("Do you want to add a course?");
+        String option = scanner.next();
+
+        while(option.equalsIgnoreCase("y") || option.equalsIgnoreCase("yes")){
+            addCourse();
+            System.out.print("Do you want to add a course? ");
+            option = scanner.next();
+        }
+    }
+
+    public static void requestTranscript(){
+
+        System.out.print("Enter Student ID: " );
+        int id = scanner.nextInt();
+
+        String query = "SELECT * FROM CS434_Enrollment WHERE Student = "  + id;
+        String result = connect.select(query, ",", "Student", "Term", "SecNum", "Grade");
+
+        System.out.print(result);
+    }
+
+    public static void computeGpa(){
+
+        System.out.print("Enter Student ID: " );
+        int id = scanner.nextInt();
+
+        String query = "SELECT GPA FROM CS434_Student WHERE id = "  + id;
+        String result = connect.select(query, "", "GPA");
+        System.out.print("Student GPA: " + result);
+
+    }
+
     public static void studentMenu(){
 
-        System.out.println("Student Functions Menu");
-        System.out.println("(1) Register for courses");
-        System.out.println("(2) Add/Drop a course");
-        System.out.println("(3) Request Transcript");
-        System.out.println("(4) Compute GPA");
-        System.out.println("(5) Quit");
-
+        showStudentMenu();
         int option = scanner.nextInt();
 
         while(true){
 
             switch(option){
+
                 case 1:
-
-                    System.out.print("Enter student username: ");
-                    String username = scanner.next();
-
-                    System.out.print("Enter student password: ");
-                    String password = scanner.next();
-
-                    String column = "id";
-
-                    String res = mysqlconn.select("SELECT id FROM CS434_Student WHERE username = '" + username + "' AND password = '" + password + "'", column);
-                    int id = Integer.parseInt(res);
-
+                    registerCourses();
+                    showStudentMenu();
                     option = scanner.nextInt();
                     break;
                 case 2:
+
+                    System.out.print("Press 1 to add a course or press 2 to drop a course: ");
+
+                    int addDropOption = scanner.nextInt();
+
+                    if(addDropOption == 1){
+                        addCourse();
+                    } else if(addDropOption == 2){
+
+                    }
+
+                    showStudentMenu();
                     option = scanner.nextInt();
                     break;
                 case 3:
+                    requestTranscript();
+                    showStudentMenu();
                     option = scanner.nextInt();
                     break;
                 case 4:
+                    computeGpa();
+                    showStudentMenu();
                     option = scanner.nextInt();
                     break;
                 case 5:
-                    System.exit(0);
+                    try {
+                        connect.close();
+                        System.exit(0);
+                    } catch(IOException io){
+                        System.err.println(io);
+                    }
                     break;
                 default:
                     System.err.println("Invalid option, please enter again");
@@ -93,15 +203,7 @@ public class Menu {
 
     public static void administrativeMenu(){
 
-        System.out.println("Administrative Functions Menu");
-        System.out.println("(1) Create a new course/drop course");
-        System.out.println("(2) Prepare term schedule (add sections)");
-        System.out.println("(3) Add/drop instructors");
-        System.out.println("(4) Alter term schedule (add/drop/update sections)");
-        System.out.println("(5) Add/drop students");
-        System.out.println("(6) Print the list of instructors and the number of courses each of them is teaching");
-        System.out.println("(7) Quit");
-
+        showAdministrativeMenu();
         int option = scanner.nextInt();
 
         while(true){
@@ -126,7 +228,12 @@ public class Menu {
                     option = scanner.nextInt();
                     break;
                 case 7:
-                    System.exit(0);
+                    try {
+                        connect.close();
+                        System.exit(0);
+                    } catch(IOException io){
+                        System.err.println(io);
+                    }
                     break;
                 default:
                     System.err.println("Invalid option, please enter again");
@@ -138,12 +245,7 @@ public class Menu {
 
     public static void reportingMenu(){
 
-        System.out.println("Administrative Functions Menu");
-        System.out.println("(1) Print schedule of classes (for a term)");
-        System.out.println("(2) Print the catalog");
-        System.out.println("(3) Print the honors list of students for a department (e.g. students who GPA > 3.8)");
-        System.out.println("(4) Quit");
-
+        showReportingMenu();
         int option = scanner.nextInt();
 
         while(true){
@@ -159,7 +261,12 @@ public class Menu {
                     option = scanner.nextInt();
                     break;
                 case 4:
-                    System.exit(0);
+                    try {
+                        connect.close();
+                        System.exit(0);
+                    } catch(IOException io){
+                        System.err.println(io);
+                    }
                     break;
                 default:
                     System.err.println("Invalid option, please enter again");
@@ -171,9 +278,15 @@ public class Menu {
 
     public static void main(String[] args){
 
-        scanner = new Scanner(System.in);
-        mysqlconn = new MysqlConnect("database.prop");
-        mainMenu();
+        try {
 
+            scanner = new Scanner(System.in);
+            connect = new MysqlConnect("database.prop");
+            mainMenu();
+            connect.close();
+
+        } catch(IOException io){
+            System.err.println(io);
+        }
     }
 }
--- MysqlConnect.java
+++ MysqlConnect.java
@@ -13,6 +13,9 @@ import java.util.Properties;
 
 public class MysqlConnect implements Closeable, Drivers {
 
+    private static final String INSERT_ROW = "INSERT INTO";
+    private static final String DELETE_ROW = "DELETE FROM";
+
     private Connection conn;
 
     public MysqlConnect(String filename){
@@ -80,7 +83,62 @@ public class MysqlConnect implements Closeable, Drivers {
         }
     }
 
-    public void insert(String query){
+    private void insert(String query, Object... columns){
+
+        try{
+
+            PreparedStatement pstmt = conn.prepareStatement(query);
+
+            for(int index = 0; index < columns.length; index++){
+
+                if(columns[index] instanceof Integer){
+                    pstmt.setInt(index+1, (Integer)columns[index]);
+                } else if(columns[index] instanceof Double){
+                    pstmt.setDouble(index+1, ((Double)columns[index]).doubleValue());
+                } else {
+                    pstmt.setString(index+1, (String)columns[index]);
+                }
+            }
+
+            pstmt.executeUpdate();
+            pstmt.close();
+
+        } catch(SQLException ex){
+            ex.printStackTrace();
+            System.err.println(ex);
+            System.exit(1);
+        }
+    }
+
+    public void insert(String tableName, String[] columnNames, Object... columnValues){
+
+        if(columnNames.length != columnValues.length){
+            System.err.println("Error: Column names and values array have to be same length");
+            return;
+        }
+
+        String query = INSERT_ROW + " " + tableName + " ";
+
+        StringBuffer cnames = new StringBuffer("(");
+        StringBuffer cvalues = new StringBuffer("(");
+
+        for(int i = 0; i < columnNames.length; i++){
+            if(i != (columnNames.length - 1)){
+                cnames.append(columnNames[i] + ",");
+                cvalues.append("?,");
+            } else {
+                cnames.append(columnNames[i]);
+                cvalues.append("?");
+            }
+        }
+
+        cnames = cnames.append(")");
+        cvalues = cvalues.append(")");
+
+        query = query + cnames.toString() + " VALUES " + cvalues.toString();
+
+        System.out.println(query);
+        insert(query, columnValues);
 
     }
 
@@ -88,9 +146,53 @@ public class MysqlConnect implements Closeable, Drivers {
 
     }
 
-    public String select(String query, String... columns){
+    public void delete(String query){
+
+        try{
+
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+            stmt.close();
+
+        } catch(SQLException ex){
+            System.err.println(ex);
+            System.exit(1);
+        }
+
+    }
+
+    public void delete(String query, Object... columns){
+
+        try{
+
+            PreparedStatement pstmt = conn.prepareStatement(query);
+
+            for(int index = 0; index < columns.length; index++){
+
+                if(columns[index] instanceof Integer){
+                    pstmt.setInt(index+1, (Integer)columns[index]);
+                } else if(columns[index] instanceof Double){
+                    pstmt.setDouble(index+1, ((Double)columns[index]).doubleValue());
+                } else {
+                    pstmt.setString(index+1, (String)columns[index]);
+                }
+
+            }
+
+            pstmt.executeUpdate();
+            pstmt.close();
+
+        } catch(SQLException ex){
+            System.err.println(ex);
+            System.exit(1);
+        }
+
+    }
+
+    public String select(String query, String delim, String... columns){
 
         String result = "";
+
         try {
 
             Statement stmt = conn.createStatement();
@@ -98,12 +200,14 @@ public class MysqlConnect implements Closeable, Drivers {
 
             while(rset.next()){
 
-                for(String column : columns){
-                    //System.out.print(rset.getString(column) + "\t");
-                    result = result + rset.getString(column);
+                for(int index = 0; index < columns.length; index++){
+                    if(index != (columns.length - 1))
+                        result = result + rset.getString(columns[index]) + delim;
+                    else
+                        result = result + rset.getString(columns[index]);
                 }
 
-                //System.out.println();
+                result = result + "\n";
             }
 
             rset.close();
@@ -111,14 +215,13 @@ public class MysqlConnect implements Closeable, Drivers {
 
         } catch(SQLException ex){
             System.err.println(ex);
-            close();
             System.exit(1);
         }
 
         return result;
     }
 
-    public void close(){
+    public void close() throws IOException {
 
         try {
             if(conn != null){
@@ -127,18 +230,31 @@ public class MysqlConnect implements Closeable, Drivers {
             }
         } catch(SQLException ex){
             System.err.println(ex);
-            System.exit(1);
         }
+    }
+
+    private static void testSelectQuery(){
+
+        String query = "SELECT * FROM CS434_Student";
+        String[] columns = {"username", "password"};
+        String[] columns2 = {"DeptName", "College"};
+
+        try {
+
+            MysqlConnect mysql = new MysqlConnect("database.prop");
+            mysql.select(query, ",", columns);
+            mysql.insert("CS434_Department", columns2, "CSTest", "CCS");
+            mysql.close();
+
+        } catch(IOException ex){
+            System.err.println(ex);
+        }
+
     }
 
     public static void main(String[] args){
 
-        String query = "SELECT * FROM CS434_Student";
-        String[] columns = {"username", "password"};
-
-        MysqlConnect mysql = new MysqlConnect("database.prop");
-        mysql.select(query, columns);
-        mysql.close();
+        testSelectQuery();
     }
 
 }
